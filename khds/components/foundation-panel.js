b04403f1d682ebffee963df8547560d12053be4f
Brian Kodawa <Brian.Kodawa@gmail.com>
2026-07-30 14:08:27 -0300

    feat: initialize KHDS foundation

---
+++ khds/components/foundation-panel.js
@@ -0,0 +1,12 @@
+/**
+ * KHDS Foundation Panel
+ *
+ * Internal design review component.
+ *
+ * Every Kodawa Labs hospitality project
+ * can mount this panel while designing.
+ */
+
+export class FoundationPanel {
+
+}
